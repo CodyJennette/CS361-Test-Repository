5aed6bf4a73f2b45ad12521755f1217a318b1ba4
CodyJennette <jennettc@oregonstate.edu>
2022-01-09 20:01:55 -0500

    Test version 1

---
+++ test greeting.py
@@ -0,0 +1,8 @@
+#  Cody Jennette (jennettc@oregonstate.edu)
+#  CS361
+#  Test commit via PyCharm
+#  GitHub username: CodyJennette
+
+greeting = "This file has been uploaded to the CS361-Test-Repository on GitHub."
+
+print(greeting)
